645eff97176d0c46a37f7824c867dca86b70ef21
RRFHOUDEN <keisuke.toyodano@gmail.com>
2020-04-25 21:19:50 +0900

    update calculate age

--- calculationAgeScript.js
+++ calculationAgeScript.js
@@ -1,12 +1,30 @@
 $(function() {
   
     // 「#form」要素のsubmitイベントを作成してください
-    $('#birthYearToAgeForm').submit(function() {
-        var textValue = parseInt( $('#year').val() );
+    $('#birth-year-to-age-form').submit(function() {
+        var textValue = parseInt( $('#birth-year').val() );
         textValue = 2020 - textValue;
         textValue = Number(textValue);
         textValue = "今年の誕生日を迎えた後" + textValue + "歳になります"
-        $('#nowAge').text(textValue);
+        $('#now-age-birth').text(textValue);
+        return false;
+    });
+
+    $('#univent-year-to-age-form').submit(function() {
+        var textValue = parseInt( $('#univent-year').val() );
+        textValue = 2020 - textValue + 19;
+        textValue = Number(textValue);
+        textValue = "今年の誕生日を迎えた後" + textValue + "歳になります"
+        $('#now-age-univent').text(textValue);
+        return false;
+    });
+
+    $('#univgrad-year-to-age-form').submit(function() {
+        var textValue = parseInt( $('#univgrad-year').val() );
+        textValue = 2020 - textValue + 23;
+        textValue = Number(textValue);
+        textValue = "今年の誕生日を迎えた後" + textValue + "歳になります"
+        $('#now-age-univgrad').text(textValue);
         return false;
     });
       
